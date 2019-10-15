607e4feaf881a05413098839a75548753f938702
王宇晗 <ousakai@wangyuhandeMacBook-Pro.local>
2019-10-16 00:36:33 +0800

    init

---
+++ src/views/homePage1.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="card">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+.container {
+  height: 100%;
+  .card {
+  height: 100%;
+    background: url('https://www.fsxhs.com/img/ind_pn2_bg.jpg') no-repeat;
+    background-size:  100% 100%;
+  }
+}
+</style>
